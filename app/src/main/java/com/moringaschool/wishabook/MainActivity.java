24dca258f556117700712df3e79f118edf6aa2d7
hamsoace <hamsoace@gmail.com>
2020-11-16 02:01:15 +0300

    ButterKnife works

--- app/src/main/java/com/moringaschool/wishabook/MainActivity.java
+++ app/src/main/java/com/moringaschool/wishabook/MainActivity.java
@@ -7,16 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btnLogin, btnSignup;
+//    Button btnLogin, btnSignup;
+    @BindView(R.id.btnLogin) Button btnLogin;
+    @BindView(R.id.btnRegister) Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnLogin=findViewById(R.id.btnLogin);
-        btnSignup=findViewById(R.id.btnRegister);
+//        btnLogin=findViewById(R.id.btnLogin);
+//        btnSignup=findViewById(R.id.btnRegister);
+        ButterKnife.bind(this);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override

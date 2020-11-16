05586db1f5c24ec4a7f4e6ba48912ceb723545f4
hamsoace <hamsoace@gmail.com>
2020-11-16 21:39:09 +0300

    sign up finished with intent

--- app/src/main/java/com/moringaschool/wishabook/MainActivity.java
+++ app/src/main/java/com/moringaschool/wishabook/MainActivity.java
@@ -31,5 +31,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

166c941db7ffecb10440b4749e7cdac690c64c6b
hamsoace <hamsoace@gmail.com>
2020-12-01 09:25:29 +0300

    Method in RecyclerViewAdapter passed

--- app/src/main/java/com/moringaschool/wishabook/RecyclerViewAdapter.java
+++ app/src/main/java/com/moringaschool/wishabook/RecyclerViewAdapter.java
@@ -1,8 +1,32 @@
 package com.moringaschool.wishabook;
 
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
 
-    public class RecyclerHolder
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class RecyclerHolder extends RecyclerView.ViewHolder{
+
+        public RecyclerHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }

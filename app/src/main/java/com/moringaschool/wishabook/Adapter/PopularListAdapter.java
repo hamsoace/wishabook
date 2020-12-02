c80036f79c935f7dc3307bbf444f5ad2f1cc245f
hamsoace <hamsoace@gmail.com>
2020-12-02 15:26:46 +0300

    Link adapter images

--- app/src/main/java/com/moringaschool/wishabook/Adapter/PopularListAdapter.java
+++ app/src/main/java/com/moringaschool/wishabook/Adapter/PopularListAdapter.java
@@ -1,13 +1,16 @@
 package com.moringaschool.wishabook.Adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.moringaschool.wishabook.Model.PopularList;
+import com.moringaschool.wishabook.R;
 
 import java.util.List;
 
@@ -19,11 +22,15 @@ public class PopularListAdapter extends RecyclerView.Adapter<PopularListAdapter.
     @NonNull
     @Override
     public PopularListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(context).inflate(R.layout.product_row_items, parent, false);
+        return new PopularListViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PopularListViewHolder holder, int position) {
+
+        holder.productImageView.setImageResource(Integer.parseInt(productList.get(position).getImageUrl()));
 
     }
 
@@ -34,8 +41,12 @@ public class PopularListAdapter extends RecyclerView.Adapter<PopularListAdapter.
 
     public static class PopularListViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView productImageView;
+
         public PopularListViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            productImageView = itemView.findViewById(R.id.productListImage);
         }
     }
 }

15b603e144d6c297d50a3001674094e5b911a9a1
hamsoace <hamsoace@gmail.com>
2020-12-02 14:12:44 +0300

    popular list adapter constructors and getters and setters

--- app/src/main/java/com/moringaschool/wishabook/Model/PopularList.java
+++ app/src/main/java/com/moringaschool/wishabook/Model/PopularList.java
@@ -4,4 +4,25 @@ public class PopularList {
 
     Integer id;
     String imageUrl;
+
+    public PopularList(Integer id, String imageUrl) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }

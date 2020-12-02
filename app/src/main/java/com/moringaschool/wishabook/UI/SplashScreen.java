fd13d07fb38bec059d0c0f643e3bef956f2f863d
hamsoace <hamsoace@gmail.com>
2020-12-03 01:32:15 +0300

    finished splash screen animation

--- app/src/main/java/com/moringaschool/wishabook/UI/SplashScreen.java
+++ app/src/main/java/com/moringaschool/wishabook/UI/SplashScreen.java
@@ -1,7 +1,13 @@
 package com.moringaschool.wishabook.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,11 +15,37 @@ import com.moringaschool.wishabook.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static int SPLASH_TIMER = 5000;
+
+    ImageView backgroundImage;
+    TextView poweredBy;
+
+    Animation sideAnim, bottomAnim;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
+
+        backgroundImage = findViewById(R.id.background_image);
+        poweredBy = findViewById(R.id.poweredBy);
+
+        sideAnim = AnimationUtils.loadAnimation(this, R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+        backgroundImage.setAnimation(sideAnim);
+        poweredBy.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMER);
+
     }
 }

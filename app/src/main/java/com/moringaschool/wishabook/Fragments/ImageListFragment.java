deb3cffdf1028d0af7ef9a030e50550c095be674
hamsoace <hamsoace@gmail.com>
2020-12-09 01:01:51 +0300

    main activity and fragments done

---
+++ app/src/main/java/com/moringaschool/wishabook/Fragments/ImageListFragment.java
@@ -0,0 +1,4 @@
+package com.moringaschool.wishabook.Fragments;
+
+public class ImageListFragment {
+}
--- app/src/main/java/com/moringaschool/wishabook/UI/MainActivity.java
+++ app/src/main/java/com/moringaschool/wishabook/UI/MainActivity.java
@@ -1,58 +1,245 @@
 package com.moringaschool.wishabook.UI;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Adapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.moringaschool.wishabook.Fragments.ImageListFragment;
 import com.moringaschool.wishabook.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static int SPLASH_TIMER = 5000;
-
-    ImageView backgroundImage;
-    TextView poweredBy;
-
-    Animation sideAnim, bottomAnim;
-
+    public static int notificationCountCart = 0;
+    static ViewPager viewPager;
+    static TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, "Open navigation drawer", "Close navigation drawer");
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-        backgroundImage = findViewById(R.id.background_image);
-        poweredBy = findViewById(R.id.poweredBy);
+        NavigationView navigationView = (NavigationView)findViewById(R.id.new_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        sideAnim = AnimationUtils.loadAnimation(this, R.anim.side_anim);
-        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+        tabLayout = (TabLayout)findViewById(R.id.tabs);
 
-        backgroundImage.setAnimation(sideAnim);
-        //poweredBy.setAnimation(bottomAnim);
+        if (viewPager !=null){
+            setupViewPager(viewPager);
+            tabLayout.setupWithViewPager(viewPager);
+        }
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },SPLASH_TIMER);
     }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onBackPressed(){
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // Get the notifications MenuItem and
+        // its LayerDrawable (layer-list)
+        MenuItem item = menu.findItem(R.id.action_cart);
+        NotificationCountSetClass.setAddToCart(MainActivity.this, item,notificationCountCart);
+        // force the ActionBar to relayout its MenuItems.
+        // onCreateOptionsMenu(Menu) will be called again.
+        invalidateOptionsMenu();
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            startActivity(new Intent(MainActivity.this, SearchResultActivity.class));
+            return true;
+        }else if (id == R.id.action_cart) {
+
+           /* NotificationCountSetClass.setAddToCart(MainActivity.this, item, notificationCount);
+            invalidateOptionsMenu();*/
+            startActivity(new Intent(MainActivity.this, CartListActivity.class));
+
+           /* notificationCount=0;//clear notification count
+            invalidateOptionsMenu();*/
+            return true;
+        }else {
+            startActivity(new Intent(MainActivity.this, EmptyActivity.class));
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        ImageListFragment fragment = new ImageListFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("type", 1);
+        fragment.setArguments(bundle);
+
+        adapter.addFragment(fragment, getString(R.string.item_1));
+        fragment = new ImageListFragment();
+        bundle = new Bundle();
+        bundle.putInt("type", 2);
+        fragment.setArguments(bundle);
+
+
+        adapter.addFragment(fragment, getString(R.string.item_2));
+        fragment = new ImageListFragment();
+        bundle = new Bundle();
+        bundle.putInt("type", 3);
+        fragment.setArguments(bundle);
+
+        adapter.addFragment(fragment, getString(R.string.item_3));
+        fragment = new ImageListFragment();
+        bundle = new Bundle();
+        bundle.putInt("type", 4);
+        fragment.setArguments(bundle);
+
+        adapter.addFragment(fragment, getString(R.string.item_4));
+        fragment = new ImageListFragment();
+        bundle = new Bundle();
+        bundle.putInt("type", 5);
+        fragment.setArguments(bundle);
+
+        adapter.addFragment(fragment, getString(R.string.item_5));
+        fragment = new ImageListFragment();
+        bundle = new Bundle();
+        bundle.putInt("type", 6);
+        fragment.setArguments(bundle);
+
+        adapter.addFragment(fragment, getString(R.string.item_6));
+        viewPager.setAdapter(adapter);
+    }
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_item1) {
+            viewPager.setCurrentItem(0);
+        } else if (id == R.id.nav_item2) {
+            viewPager.setCurrentItem(1);
+        } else if (id == R.id.nav_item3) {
+            viewPager.setCurrentItem(2);
+        } else if (id == R.id.nav_item4) {
+            viewPager.setCurrentItem(3);
+        } else if (id == R.id.nav_item5) {
+            viewPager.setCurrentItem(4);
+        }else if (id == R.id.nav_item6) {
+            viewPager.setCurrentItem(5);
+        }else if (id == R.id.my_wishlist) {
+            startActivity(new Intent(MainActivity.this, WishlistActivity.class));
+        }else if (id == R.id.my_cart) {
+            startActivity(new Intent(MainActivity.this, CartListActivity.class));
+        }
+
+        else if(id == R.id.my_account)
+        {
+            startActivity(new Intent(MainActivity.this, MyAccount.class));
+        }
+
+        else if(id == R.id.my_orders)
+        {
+            startActivity(new Intent(MainActivity.this, MyOrder.class));
+        }
+
+        else {
+            startActivity(new Intent(MainActivity.this, EmptyActivity.class));
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+
 }
